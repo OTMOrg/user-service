683b56fb40b4e3585922060cd603acfd25a986cd
Sanket Vetkoli <vetkolisanket@gmail.com>
2019-08-16 15:37:18 +0530

    Ignored empty user service test, removed .vscode directory which was commited before

--- src/test/java/com/standapp/api/userservice/service/UserServiceTest.java
+++ src/test/java/com/standapp/api/userservice/service/UserServiceTest.java
@@ -1,9 +1,11 @@
 package com.standapp.api.userservice.service;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
+@Ignore
 public class UserServiceTest {
 
     
